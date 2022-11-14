dfb85ac7aa6c2fe003890f3e37760919b3f0c5d6
Raveena <itraveena@gmail.com>
2022-11-14 18:48:43 +0000

    my first variable coding

---
+++ print.sh
@@ -0,0 +1,8 @@
+#! /bin/sh
+NAME="ZARA ALI" 
+readonly NAME
+NAME="Devinder"
+
+==================================
+my doubt is that if i will remove the 2nd name then it is giving me the another output
+
