2f2abc5845dbcf3fd642b5f2bcd55b9883e1601c
Vikas Singh <itvika@gmail.com>
2022-11-16 19:15:33 +0530

    relational operator

---
+++ relational.sh
@@ -0,0 +1,32 @@
+read a b
+if [ $a -gt $b ]
+then
+	echo a is greater than b
+fi
+
+if [ $a -ge $b ]
+then
+        echo a is greater than or equal to b
+fi
+
+if [ $a -lt $b ]
+then
+        echo a is less than b
+fi
+
+if [ $a -le $b ]
+then
+        echo a is less than or equal to b
+fi
+
+if [ $a -eq $b ]
+then
+        echo a is equal to b
+fi
+
+if [ $a -ne $b ]
+then
+        echo a is not equal to b
+fi
+
+
