b476a8e5a0cb9c1a0ed156abd5875d517ffa8e6d
Vikas Singh <itvika@gmail.com>
2022-11-17 23:08:42 +0530

    sdfs

---
+++ div-5-11-not.sh
@@ -0,0 +1,24 @@
+read num
+
+if [ `expr $num % 5` -eq 0 -a `expr $num % 11` -eq 0 ]
+then
+	echo "deivisible by 5 and 11"
+else
+	echo "not divisible by 5 and 11"
+fi
+
+
+
+echo ========second way=========
+if [ `expr $num % 5` -eq 0 ]
+then 
+	if [ `expr $num % 11` -eq 0 ]
+	then
+		echo "deivisible by 5 and 11"
+	else
+		echo "not divisible by 5 and 11"
+	fi
+else
+	echo "only divisible by 5 "
+fi	
+	
